210439538095c21bb1bfb0a557d00979e24643d2
jannunzi <jannunzi@gmail.com>
2020-11-01 09:47:56 -0500

    added sections, students, enrollments

---
+++ src/main/java/com/example/springbootwebjpatemplateproject/models/Section.java
@@ -0,0 +1,60 @@
+package com.example.springbootwebjpatemplateproject.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="sections")
+public class Section {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer sectionId;
+    private String sectionName;
+    @ManyToOne
+    @JsonIgnore
+    private Course course;
+
+    @OneToMany(mappedBy="section")
+    private List<Enrollment> students;
+
+    public void addStudent(Student student, Float grade) {
+        Enrollment enrollment = new Enrollment();
+        enrollment.setStudent(student);
+        enrollment.setSection(this);
+        enrollment.setStudentId(student.getStudentId());
+        enrollment.setSectionId(this.getSectionId());
+        enrollment.setGrade(grade);
+        if(this.students == null) {
+            this.students = new ArrayList<>();
+        }
+        this.students.add(enrollment);
+        student.getSections().add(enrollment);
+    }
+
+    public Integer getSectionId() {
+        return sectionId;
+    }
+
+    public void setSectionId(Integer sectionId) {
+        this.sectionId = sectionId;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
